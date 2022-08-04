d09b21a6e60b61d9ed6c157f6d754bd3f5e14aa7
Bernard Chang <changhuahong@gmail.com>
2022-08-05 00:22:45 +0800

    Push signup to Segment Personas

--- transform.js
+++ transform.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const sgMail = require('@sendgrid/mail');
 const sgClient = require('@sendgrid/client');
+const Analytics = require('analytics-node');
 const express = require('express')
 const app = express();
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 sgClient.setApiKey(process.env.SENDGRID_API_KEY);
+var analytics = new Analytics(process.env.SEGMENT_WRITE_KEY);
 
 app.use(express.urlencoded({
     extended: true
@@ -13,17 +15,61 @@ app.use(express.urlencoded({
 app.use(express.json());
 app.set('view engine', 'ejs');
 
+app.listen(3000, () => { // Tells the app to start on port 3000. This function below is run when
+    console.log("Server listening on port 3000!"); // Say in the console "Server listening on port 3000!"
+})
 
-app.get("/", (req, res) => { // Creates sort of a listener for when there are "GET" requests to the "/" (root) path. Takes in req (request) and res (response)
-    res.send("Hello World");
-});
-
-app.get('/signup', (req, res) => {
+app.get('/', (req, res) => {
     res.render('signup', signUpPage);
 });
-
 
 const signUpPage = {
     title: 'Join Our Newsletter',
     subtitle: 'Subscribe to our newsletter to receive the latest news and products.',
 };
+
+app.post('/signup', async (req, res) => {
+
+    const confNum = randNum();
+    const params = new URLSearchParams({
+        conf_num: confNum,
+        email: req.body.email,
+    });
+
+    const confirmationURL = req.protocol + '://' + req.headers.host + '/confirm/?' + params;
+
+    const msg = {
+        to: req.body.email,
+        from: 'hello@twi-lio.com',
+        subject: `Confirm your subscription to our newsletter`,
+        html: `Hello ${req.body.firstname},<br>Thank you for subscribing to our newsletter. Please complete and confirm your subscription by <a href="${confirmationURL}"> clicking here</a>.`
+    }
+
+    let traits = {
+        firstName: req.body.firstname,
+        lastName: req.body.lastname,
+        email: req.body.email,
+        phone: req.body.phone,
+        contactPreference: req.body.contactpreference,
+        confNum: confNum,
+        createdAt: new Date()
+    }
+
+    // Push to Segment Personas
+    await pushContact(req.body.email, traits);
+    await sgMail.send(msg);
+    res.render('message', { message: 'Thank you for signing up for our newsletter! Please complete the process by confirming the subscription in your email inbox.' });
+});
+
+
+function randNum() {
+    return Math.floor(Math.random() * 90000) + 10000;
+}
+
+async function pushContact(userId, traits) {
+    analytics.identify({
+        userId: userId,
+        traits: traits
+    });
+}
+
